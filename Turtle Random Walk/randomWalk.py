a0f4fab70d60ded97a29a65b752a364c3f683969
Tron51226 <70756241+Tron51226@users.noreply.github.com>
2022-03-30 12:21:55 +0530

    Random Walk in Python
    Program to generate a 'random walk', using the turtle module in python, and using colour palettes from pieces of album art (extracted using the colorgram.py module)

---
+++ Turtle Random Walk/randomWalk.py
@@ -0,0 +1,42 @@
+from turtle import Turtle
+from turtle import Screen
+import colorgram
+import random
+
+screen = Screen()
+
+
+t = Turtle()
+t.speed('fastest')
+t.ht()
+t.width(15)
+screen.colormode(255)
+
+
+colors = colorgram.extract('Turtle Random Walk\images\inRainbowsArt.jpg',30)
+rgbColors = []
+for c in colors:
+    if sum((c.rgb.r ,c.rgb.g, c.rgb.b)) > 250:
+        rgbColors.append((c.rgb.r ,c.rgb.g, c.rgb.b))
+
+
+
+turns = [0, 90, 180, 270]
+for _ in range(0, 1001):
+    #clamp turtle within bounds
+    if t.xcor() >= 250:
+        t.setheading(180)
+    elif t.xcor() <= -250:
+        t.setheading(0)
+
+    if t.ycor() >= 250:
+        t.setheading(270)
+    elif t.ycor() <= -250:
+        t.setheading(90)
+
+    t.pencolor(random.choice(rgbColors))
+    t.forward(25)
+    t.right(random.choice(turns))
+
+
+screen.exitonclick()
